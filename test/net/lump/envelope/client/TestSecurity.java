d66b8523d50028a2be9ab3bb8bc8ad7690ba0c78
Troy Bowman <troy@lump.net>
2007-08-18 07:56:24 +0000

    whoops

--- test/net/lump/envelope/client/TestSecurity.java
+++ test/net/lump/envelope/client/TestSecurity.java
@@ -2,13 +2,10 @@ package us.lump.envelope.client;
 
 import junit.framework.TestCase;
 import us.lump.envelope.Command;
-import us.lump.envelope.Command.Name;
-import us.lump.envelope.Command.Param;
 import us.lump.envelope.TestSuite;
 import us.lump.envelope.client.portal.SecurityPortal;
 import us.lump.envelope.client.ui.prefs.LoginSettings;
 import us.lump.envelope.server.rmi.Controller;
-import us.lump.lib.util.Encryption;
 
 import java.security.KeyPair;
 
@@ -32,22 +29,31 @@ public class TestSecurity extends TestCase {
     assertTrue("User does not auth", authed);
 
     // an entity object retrieval test
-    Command cmd = new Command(Name.listTransactions)
-            .set(Param.year, 2007).sign(user, kp.getPrivate());
+//    Command cmd = new Command(Command.Name.listTransactions)
+//            .set(Command.Param.year, 2007).sign(user, kp.getPrivate());
 
-    System.out.println("Encryption.decodePrivateKey(\""
-            + Encryption.encodeKey(kp.getPrivate()) + "\");");
+//    System.out.println("Encryption.decodePrivateKey(\""
+//            + Encryption.encodeKey(kp.getPrivate()) + "\");");
 
-    System.out.println("Encryption.decodePublicKey(\""
-            + Encryption.encodeKey(kp.getPublic()) + "\");");
+//    System.out.println("Encryption.decodePublicKey(\""
+//            + Encryption.encodeKey(kp.getPublic()) + "\");");
 
-    System.out.println("((Command)Encryption.thaw(us.lump.lib.util.Base64.base64ToByteArray(\"" +
-            us.lump.lib.util.Base64.byteArrayToBase64(Encryption.freeze(cmd)) +
-            "\"))).verify(kp.getPublic());");
+//    System.out.println("((Command)Encryption.thaw("
+//            + "us.lump.lib.util.Base64.base64ToByteArray(\""
+//            + us.lump.lib.util.Base64
+//            .byteArrayToBase64(Encryption.freeze(cmd))
+//            + "\"))).verify(kp.getPublic());");
 
 //    List<Identifiable> list = (List<Identifiable>) controller.invoke(cmd);
 //    System.out.println(list.size());
-    System.out.println(controller.invoke(new Command(Command.Name.authedPing).sign(user, kp.getPrivate())));
+
+    try {
+      System.out.println(controller
+              .invoke(new Command(Command.Name.authedPing)
+              .sign(user, kp.getPrivate())));
+    } catch(Exception e) {
+      fail("Exception thrown");
+    }
   }
 
   protected void setUp() throws Exception {
