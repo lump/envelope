8687045d527d9cd9e9e11be5fa26576c1395be6e
Troy Bowman <troy@lump.net>
2008-07-03 06:20:49 +0000

    ofx back-burnered for now

--- test/net/lump/envelope/server/ofx/TestBalance.java
+++ test/net/lump/envelope/server/ofx/TestBalance.java
@@ -1,9 +1,9 @@
 package us.lump.envelope.server.ofx;
 
 import junit.framework.TestCase;
-import net.ofx.types.OFX;
-import net.ofx.types.SignonRequest;
-import net.ofx.types.SignonRequestMessageSetV1;
+//import net.ofx.types.OFX;
+//import net.ofx.types.SignonRequest;
+//import net.ofx.types.SignonRequestMessageSetV1;
 import org.junit.Test;
 
 import javax.xml.bind.JAXBContext;
@@ -20,26 +20,26 @@ import java.io.FileOutputStream;
  * .
  *
  * @author Troy Bowman
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class TestBalance extends TestCase {
   //http://java.sun.com/developer/technicalArticles/WebServices/jaxb/index.html
   @Test
-  public void testLogin() throws Exception {
-    OFX ofx = new OFX();
-
-    SignonRequest srq = new SignonRequest();
-    srq.setUSERID("4166559");
-    srq.setUSERPASS("blah");
-    SignonRequestMessageSetV1 srqm = new SignonRequestMessageSetV1();
-    srqm.setSONRQ(srq);
-    ofx.setSIGNONMSGSRQV1(srqm);
-
-    JAXBContext jaxbContext = JAXBContext.newInstance("net.ofx.type");
-    Marshaller marshaller = jaxbContext.createMarshaller();
-    marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, new Boolean(true));
-    marshaller.marshal(ofx, new FileOutputStream("OFX.xml"));
-  }
+//  public void testLogin() throws Exception {
+//    OFX ofx = new OFX();
+//
+//    SignonRequest srq = new SignonRequest();
+//    srq.setUSERID("4166559");
+//    srq.setUSERPASS("blah");
+//    SignonRequestMessageSetV1 srqm = new SignonRequestMessageSetV1();
+//    srqm.setSONRQ(srq);
+//    ofx.setSIGNONMSGSRQV1(srqm);
+//
+//    JAXBContext jaxbContext = JAXBContext.newInstance("net.ofx.type");
+//    Marshaller marshaller = jaxbContext.createMarshaller();
+//    marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, new Boolean(true));
+//    marshaller.marshal(ofx, new FileOutputStream("OFX.xml"));
+//  }
 
   protected void setUp() throws Exception {
     super.setUp();
