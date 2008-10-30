67034126bdd04b7f4f2acc45bae9e94adfbb7e99
Troy Bowman <troy@lump.net>
2008-10-30 21:46:41 +0000

    maybe this would work as an enum.

--- src/net/lump/lib/util/Revision.java
+++ src/net/lump/lib/util/Revision.java
@@ -4,31 +4,43 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Utility class for programmatic access to cvs variables.
+ * Utility class for enum access to cvs variables.
  *
  * @author Troy Bowman
- * @version $Id: Revision.java,v 1.1 2008/10/30 21:30:33 troy Exp $
+ * @version $Id: Revision.java,v 1.2 2008/10/30 21:46:41 troy Exp $
  */
-public class Revision {
+public enum Revision {
 
-  private static final String[] REVS = new String[]{
+  Name,
+  State,
+  Revision,
+  Date,
+  Author;
+
+  // store the value from which cvs gives us.
+  private String value = null;
+
+  // get CVS to fill in the values in strings
+  private final String[] REVS = new String[]{
       "$Name:  $",
       "$State: Exp $",
+      "$Revision: 1.2 $",
+      "$Date: 2008/10/30 21:46:41 $",
+      "$Author: troy $"
   };
 
-  public String getRevision() {
-    String version = null;
+  private Revision() {
+    Pattern p = Pattern.compile("^\\$" + this.toString() + ":\\s+(.+?)\\s*$");
+    Matcher m = p.matcher(REVS[ordinal()]);
+    if (m.matches()) value = m.group(1);
+  }
 
-    Pattern p = Pattern.compile("^\\$(Name|State|Revision):\\s+(.+?)\\s*$");
-
-    for (String s : REVS) {
-      Matcher m = p.matcher(s);
-      if (m.matches()) {
-        version = m.group(1);
-        break;
-      }
-    }
-
-    return version;
+  /**
+   * Retrieve the String value.
+   *
+   * @return String
+   */
+  public String value() {
+    return value;
   }
 }
